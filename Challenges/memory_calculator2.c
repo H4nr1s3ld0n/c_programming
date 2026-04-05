f74f10ddfa0b534fd57f909a4166a39784982dbd
H4nr1s3ld0n <p3pit0@tutamail.com>
2026-04-05 11:08:23 +0000

    Create memory_calculator2.c

---
+++ Challenges/memory_calculator2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main() {
+    int quantity = 0;
+    int bytes = 0;
+    int kilobytes = 0;
+    int megabytes = 0;
+    char type;
+    
+    scanf("%c %d", &type, &quantity);
+    
+    if(type == 'i'){
+        bytes += quantity * sizeof(int);
+    } 
+    else if (type == 'd'){
+        bytes += quantity * sizeof(double);
+    }
+    else if (type == 'c'){
+        bytes += quantity * sizeof(char);
+    }
+    
+    if(bytes>=1000000){  // Checking if there are any MB and converting
+        megabytes = bytes/1000000;
+        bytes %= 1000000;
+        
+        if(bytes>=1000){
+            kilobytes = bytes/1000;
+            bytes %= 1000;
+            printf("%d MB and %d KB and %d B", megabytes, kilobytes, bytes);
+        }else{
+        printf("%d MB and %d B", megabytes, bytes);
+        return 0;
+        }
+    }
+    
+    if(bytes>=1000)   // Converting the bytes to KB and checking if there are any.
+    {
+        kilobytes = bytes/1000;
+        bytes %= 1000;
+        printf("%d KB and %d B", kilobytes, bytes);
+        return 0;
+    }
+    
+    printf("%d B", bytes);
+    return 0;
+}
