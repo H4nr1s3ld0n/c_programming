9102416590b2b1e4a2ce24c5d86ed1b59c03f064
H4nr1s3ld0n <p3pit0@tutamail.com>
2026-04-15 07:31:12 +0000

    Create list_student1.c

---
+++ Challenges/list_student1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct student {
+      char name[50];
+      int age;
+      struct student *next;
+};
+
+struct student *createStudent(char studentName[], int studentAge);
+char* copyStr(char copy[], char studentName[]);
+
+int main(void) {
+    struct student *studptr;
+    int myAge;
+    char myName[50];
+    scanf("%s %d", myName, &myAge);
+    studptr = createStudent(myName, myAge);
+    printf("New student created: %s is %d years old.\n", studptr->name, studptr->age);
+    free(studptr);
+    return 0;
+}
+
+struct student * createStudent(char studentName[], int studentAge){
+    struct student *ptr;
+    ptr = (struct student *) malloc(sizeof(struct student));
+    copyStr(ptr->name, studentName);
+    ptr->age = studentAge;
+    return ptr;
+}
+
+char* copyStr(char copy[], char studentName[]){
+    int i;
+    for (i = 0; studentName[i] != '\0'; i++) {
+        copy[i] = studentName[i];
+        
+    }
+    copy[i] = '\0'; 
+    return (char*)copy;
+}
